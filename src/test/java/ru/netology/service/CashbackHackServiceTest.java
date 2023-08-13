6992ee36fc37510441a9341db662a876caca84eb
msAleks <aleksandra.96n1@gmail.com>
2023-08-13 10:00:19 +0300

    Error2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,36 +1,36 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
-        int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain2() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
 
-        int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain3() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
-        int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 }
