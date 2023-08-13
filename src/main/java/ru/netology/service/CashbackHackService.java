d6b3ba31ff4a1a5e914f34256f996fa2ee84ff67
msAleks <aleksandra.96n1@gmail.com>
2023-08-13 09:23:42 +0300

    Error

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,13 +1,11 @@
 package ru.netology.service;
 
 public class CashbackHackService {
-    public static void main(String[] args) {
 
+    private final int boundary = 1000;
 
-        private final int boundary = 1000;
-
-        public int remain ( int amount) {
-            return boundary - amount % boundary;
-        }
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
 }
+
